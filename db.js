20f10781afee369f0300abd040bf9c1d9d916a79
OnatEngin <onatengin22@gmail.com>
2023-07-11 15:06:52 +0300

    modified

--- db.js
+++ db.js
@@ -1,29 +1,40 @@
 //database manipulasyonu icin burayi kullanacagim
 
+
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./wwdata.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
+function qry_msg () {
+
+    const db = new sqlite3.Database('./wwdata.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("DB connection successfull"); 
+    });
+
+    //db.run("INSERT INTO messages(body) VALUES ('${msg}')");
+
+    /*
+    db.get(imq, (err, row) => {
+        if (err) {
         return console.error(err.message);
-    }
-    console.log("DB connection successfull"); 
-});
+        }
+        return row
+        ? console.log(row.id, row.name)
+        : console.log(`burada bisey yok`);
+    
+    });
+    */
 
-let sql = 'SELECT id, name FROM contacts';
+    console.log("DB ACİK QRY FONKSIYONU CALISIYOR");
 
-db.get(sql, (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    return row
-      ? console.log(row.id, row.name)
-      : console.log(`burada bisey yok`);
-  
-  });
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("DB closed");
+    });
 
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log("DB closed");
-});
+}
+
+module.exports = qry_msg;
